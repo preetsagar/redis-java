969ef19db9749983675dd91e7d522effc0c6a078
preetsagar <ps792001@gmail.com>
2026-09-07 06:19:35 +0530

    MASTER : Configure listening port #bw1

--- src/main/java/io/codecrafters/redis/Main.java
+++ src/main/java/io/codecrafters/redis/Main.java
@@ -2,9 +2,19 @@ package io.codecrafters.redis;
 
 public class Main {
 
-    private static final int PORT = 6379;
+    private static final int DEFAULT_PORT = 6379;
 
     public static void main(String[] args) {
-        new RedisServer(PORT).start();
+        new RedisServer(parsePort(args)).start();
+    }
+
+    // Supports: --port <n>  (defaults to 6379)
+    private static int parsePort(String[] args) {
+        for (int i = 0; i + 1 < args.length; i++) {
+            if (args[i].equals("--port")) {
+                return Integer.parseInt(args[i + 1]);
+            }
+        }
+        return DEFAULT_PORT;
     }
 }
